6cda2df09c23e666f6303f4dd1efcb1d94b6a2e3
Leovisvuong <141708660+Leovisvuong@users.noreply.github.com>
2024-09-14 14:13:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Part 1 Print a string to the console
+//hint: Console.WriteLine("Hello World!");
+Console.WriteLine("Hi, what's your name?");
+//Part 2 Get user input and print it to the console
+//hint: Console.ReadLine();
+var name=Console.ReadLine();
+Console.WriteLine("Hi "+name+", nice to meet you.\nHow old are you?");
+var age=Console.ReadLine();
+//Part 3 Get two inputs from the user and print them on separate lines
+//hint: use \n to create a new line
+Console.WriteLine("So your name is "+name+" and you are now "+age+" years old, awesome!");
